d41dcaf96eaa3b6240cfc294bb459e1a78a6944a
saadginf <saadabbou89@gmail.com>
2019-10-14 02:30:12 +0200

    gestion classe

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 public class Test {
@@ -7,6 +9,7 @@ public class Test {
     public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
+        Scanner scc = new Scanner(System.in);
 /*
         System.out.println("donnez le nombre 1");
         int nb1 = sc.nextInt();
@@ -96,23 +99,25 @@ public class Test {
         System.out.println("résultat "+resultat);
 
  */
-        List<Integer> saisie = new ArrayList<>();
+  /*      List<Integer> saisie = new ArrayList<>();
         int rep;
         int t = 0;
         int i = 0;
         do {
-
             System.out.println("Donnez un nombre");
             saisie.add(sc.nextInt());
             i= t-1;
             if(t!=0) {
                 System.out.println(i+";"+t);
-                while(i>0 && saisie.get(t) < saisie.get(i)){
+               i = t;
+                while(i>=0 && saisie.get(t) < saisie.get(i)){
+                    saisie.add(i, saisie.get(t));
+                    saisie.remove(t+1);
                     i--;
+
                     System.out.println(i+";;"+t);
                 };
-
-                if (i>=0 && saisie.get(t) < saisie.get(i)){
+              if (i>=0 && saisie.get(t) < saisie.get(i)){
                     System.out.println(i+";;;"+t);
                     saisie.add(i, saisie.get(t));
                     saisie.remove(t+1);
@@ -121,19 +126,152 @@ public class Test {
                     saisie.add(i+1, saisie.get(t));
                     saisie.remove(t+1);
                 }
-
-
                 t= t+1;
-
             } else {
                 t = t + 1;
             }
-
             System.out.println("vous voulez insérer un autre nombre 1/0");
             rep = sc.nextInt();
-
         } while (rep == 1);
         System.out.println(saisie);
+
+  List<Integer> listeA = new ArrayList<>();
+  List<Integer> listeB = new ArrayList<>();
+  List<Integer> listeT = new ArrayList<>();
+
+        int nbA=0;
+  int nbB=0;
+  int rep;
+        System.out.println("REmplissage de la liste A");
+        do {
+            System.out.println("give a number");
+            listeA.add(sc.nextInt());
+            nbA ++;
+            System.out.println("Nouveau numero? 1/0");
+            rep = sc.nextInt();
+        } while(rep == 1);
+        rep =0;
+        System.out.println("---------------------REmplissage de la liste B-----------------------------------------");
+        do {
+            System.out.println("give a number");
+            listeB.add(sc.nextInt());
+            nbB ++;
+            System.out.println("Nouveau numero? 1/0");
+            rep = sc.nextInt();
+        } while(rep == 1);
+        System.out.println("--------------------------------listeA--------------------------------");
+        System.out.println(listeA);
+        System.out.println("---------------------------------ListeB--------------------------");
+        System.out.println(listeB);
+int min;
+int i=0;
+int j=0;
+if (nbA<nbB){
+    min = nbA;
+} else min = nbB;
+while (i<min || j<min){
+    if (listeA.get(i)<listeB.get(j)){
+        listeT.add(listeA.get(i));
+        i++;
+    } else {
+        listeT.add(listeB.get(j));
+        j++;
+    }
+}
+
+
+
+        System.out.println("-----------------------------ListeT--------------------------");
+        System.out.println(listeT);
+*/
+        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                         GESTION DES NOTES DES ELEVES");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("Donnez le nombre d'eleve");
+        int nbE = sc.nextInt();
+        sc.nextLine(); //throw away the \n not consumed by nextInt()
+        System.out.println("Donnez le nombre de matière");
+
+        int nbM = sc.nextInt();
+        sc.nextLine(); //throw away the \n not consumed by nextInt()
+
+        int gestion[][] = new int[nbE + 3][nbM+1];
+        String eleve[] = new String[nbE+1];
+        String matiere[] = new String[nbM];
+        System.out.println("gestion"+gestion.length);
+        for(int i=0; i< nbE + 3; i++){
+            gestion[i][nbM] = 0;
+        }
+        for(int i=0; i< nbM+1; i++){
+            gestion[nbE][i] = 25;
+            gestion[nbE+1][i] = 0;
+            gestion[nbE+2][i] = 0;
+        }
+        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                         SAISIE DES NOMS DES ELEVES");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+
+
+        for(int i=0; i< nbM; i++){
+
+           System.out.println("Saisissez les noms de l'eleve "+i);
+           eleve[i] = sc.nextLine();
+
+        }
+
+
+        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                         SAISIE DES MATIERES");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+
+
+        for(int i=0; i< nbM; i++){
+
+            System.out.println("Saisissez les noms de la matiere "+i);
+            matiere[i] = sc.nextLine();
+
+        }
+        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                         SAISIE DES NOTES");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+
+        for(int i=0; i< nbE; i++){
+
+
+            System.out.println("    "+eleve[i]);
+            System.out.println("--------------------");
+            for(int j=0; j< nbM; j++){
+                System.out.print(matiere[j]+" : ");
+                gestion[i][j] = sc.nextInt();
+                gestion[i][nbM] = gestion[i][nbM] + gestion[i][j]/nbM;
+                if (gestion[i][j] < gestion[nbE][j]) gestion[nbE][j] = gestion[i][j];
+                if (gestion[i][j] > gestion[nbE+1][j]) gestion[nbE+1][j] = gestion[i][j];
+                gestion[nbE+2][j] = gestion[nbE+2][j] + gestion[i][j]/nbE;
+            }
+
+
+        }
+        System.out.println("Ce programme permet d'afficher :");
+        System.out.println("1. Les notes des elevre par matières et leure moyenne");
+        System.out.println("2. Note minimale, maximale et moyenne pour chaque matière");
+        System.out.println("entrer le numéro de votre choix");
+        int choix;
+        choix = sc.nextInt();
+        if (choix == 1){
+                for (int i=0; i<nbM;i++) System.out.print("            " +matiere[i]);
+            System.out.printf("             Moyenne");
+            for (int i=0; i<nbE;i++){
+                System.out.println();
+                System.out.print(eleve[i]);
+                for (int j=0; j<nbM+1;j++){
+                    System.out.print("        " +gestion[i][j]+"         ");
+
+                }
+
+            }
+        }
+
 
     }
 
